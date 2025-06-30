85f0917db63a22e4c223e43bd9713b7bcc7bd9f4
Emre-Oktay <emreoktaybuldu@gmail.com>
2025-06-30 16:28:06 +0300

    feat: add footer and wrap game component with main tag

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,14 @@ function App() {
             <header>
                 <h1>Memory Game</h1>
             </header>
-            <Game />
+            <main>
+                <Game />
+            </main>
+            <footer>
+                <p>
+                    Made using <a href="https://api.artic.edu"> The Art Institute of Chicago API </a>
+                </p>
+            </footer>
         </>
     );
 }
